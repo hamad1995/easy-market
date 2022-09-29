70fbd817bc6851da9c13567f0ef6e03f4b4209e8
Hammad AlHammad <h.alhammad@tamkeentech.sa>
2022-09-29 17:16:12 +0300

    adding dump changes

--- backend/l-backend/routes/web.php
+++ backend/l-backend/routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [\App\Http\Controllers\ProductController::class,'index']);
